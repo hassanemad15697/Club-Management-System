bb6b8020448dffe309bcc0e47566ec5aaa5868b4
Hassan Askar <hassanemad15697@gmail.com>
2022-01-14 15:53:13 +0100

    creating project layers (DAo,Services,Controller)

---
+++ src/com/spring/DAO/PlayerDAO_Impl.java
@@ -0,0 +1,8 @@
+package com.spring.DAO;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PlayerDAO_Impl implements PlayerDAO{
+
+}
---
+++ src/com/spring/service/PlayerService_Impl.java
@@ -0,0 +1,9 @@
+package com.spring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerService_Impl implements PlayerService {
+
+
+}
